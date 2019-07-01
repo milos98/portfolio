4b0c6599fd32c8a107ea3967ad89cdad9a6f049a
Miloš Janković <24883845+milos98@users.noreply.github.com>
2019-07-02 00:08:02 +0200

    slider and logo

--- js/javascript.js
+++ js/javascript.js
@@ -12,12 +12,11 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("project");
-  var dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("project");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
@@ -25,4 +24,18 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+
+let slide = $("#slides");
+if (screen.width < 769) {
+
+  slide.on('swipeleft', function (e) { 
+    e.preventDefault();
+    slide.slideIt({direction:"left"});
+  });
+
+  slide.on('swiperight', function (e) {
+    e.preventDefault(); 
+    slide.slideIt({direction:"right"});
+  });
 }
